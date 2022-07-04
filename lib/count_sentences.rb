bdae2387082ab85b7c065777685e3dfd9cc50d8d
Lori Henderson <lorim95@gmail.com>
2022-07-04 12:52:48 -0400

    commit tree 8cfc68a7eaf994ac2258e84a45253d76e9934063

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # self.end_with?("") == false
+    self.end_with?(".") == true
   end
 
   def question?
-
+    # self.end_with?("") ==  false
+    self.end_with?("?") == true
   end
 
   def exclamation?
-
+    # self.end_with?("") == false
+    self.end_with?("!") == true
   end
 
   def count_sentences
-
+    self.split(/[\.\?\!]+/).count
   end
 end
